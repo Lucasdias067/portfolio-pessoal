f16680697590981f6ade1d7e476a21f28533bccf
Lucas Dias <lucasdias067@gmail.com>
2023-05-05 19:39:38 -0300

    adding menu for mobile

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -1,25 +1,53 @@
+"use client"
 import Link from "next/link"
 import "./style.css"
-
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useState } from "react"
 export default function Header() {
 
-  const linkHeader = [
+  const [menu, setMenu] = useState(false);
+
+  const navHeader = [
     { name: "Sobre Mim", href: "#aboutMe", className: "link-header" },
     { name: "Meus Projetos", href: "#projectList", className: "link-header" },
     { name: "Habilidades", href: "#skills", className: "link-header" },
     { name: "Contatos", href: "#contact", className: "link-header projetos-btn" }
   ]
 
+  function openMenu() {
+    setMenu(!menu)
+  }
+
   return (
     <header id="header">
-      <Link href="/" className="link-name">
-        <h1>Portfólio</h1>
-      </Link>
-      <div className="navigation_header">
-        {linkHeader.map(({ name, href, className }) => (
-          <a href={href} className={className} key={name}>{name}</a>
-        ))}
+      <div className="header">
+        <Link href="/" className="link-name">
+          <h1>Portfólio</h1>
+        </Link>
+        <div className="navigation_header">
+          {navHeader.map(({ name, href, className }) => (
+            <a href={href} className={className} key={name}>{name}</a>
+          ))}
+        </div>
+      </div>
+
+      <div className="headerMobile">
+        <div className="menuMobile">
+          <Link href="/" className="link-name">
+            <h1>Portfólio</h1>
+          </Link>
+          <FontAwesomeIcon icon={faBars} size="xl" onClick={openMenu} />
+        </div>
+        {menu &&
+          <div className="navigation_header">
+            {navHeader.map(({ name, href, className }) => (
+              <a href={href} className={className} key={name}>{name}</a>
+            ))}
+          </div>
+        }
       </div>
     </header>
   )
 }
+
